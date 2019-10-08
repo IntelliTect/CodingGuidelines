171eec15f1147f9773edd6f0fd7e0763a56e5f2c
qtuu <8949946+qtuu@users.noreply.github.com>
2019-10-08 19:55:10 +0200

    Implemented AttributeOnSeparateLine-Analyzer and CodeFix (resolves issue #20) (#32)
    * Implemented Analyzer and CodeFix
    
    added unittests to ensure proper function
    
    * fixing azure-pipelines build erros

---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer.Test/AttributesOnSeparateLinesTests.cs
@@ -0,0 +1,242 @@
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace IntelliTectAnalyzer.Tests
+{
+    [TestClass]
+    public class AttributesOnSeparateLinesTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void ProperlySetAttributes_TwoAttributesOnMethod_NoDiagnosticInformationReturned()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    class Program
+    {
+        [A]
+        [B]
+        static void Main()
+        {
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void MethodAttributeLineViolation_TwoAttributesOnSameLine_Warning()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    class Program
+    {
+        [A][B]
+        static void Main()
+        {
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(test, GetExpectedDiagnosticResult(15, 13));
+        }
+
+        [TestMethod]
+        public void ClassAttributeLineViolation_TwoAttributesOnSameLine_Warning()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+        [A][B]
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(test, GetExpectedDiagnosticResult(13, 13));
+        }
+
+        [TestMethod]
+        public void PropertyAttributeLineViolation_TwoAttributesOnSameLine_Warning()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        [A][B]
+int Prop {get;set;}
+    }
+}";
+            VerifyCSharpDiagnostic(test, GetExpectedDiagnosticResult(19, 13));
+        }
+
+  
+        [TestMethod]
+        public async Task ClassAttributeLineViolation_CodeFix_TwoAttributesOnSameLine_TwoArgumentsOnSeparateLines()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    [A][B]
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}";
+
+            var fixTest = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    [A]
+    [B]
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}";
+            await VerifyCSharpFix(test, fixTest);
+        }
+
+        [TestMethod]
+        public async Task ClassAttributeListLineViolation_CodeFix_TwoAttributesOnSameLine_TwoArgumentsOnSeparateLines()
+        {
+            var test = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    [A, B]
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}";
+
+            var fixTest = @"using System;
+
+namespace ConsoleApp
+{
+    class AAttribute : Attribute
+    {
+    }
+
+    class BAttribute : Attribute
+    {
+    }
+
+    [A]
+    [B]
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}";
+            await VerifyCSharpFix(test, fixTest);
+        }
+
+        private DiagnosticResult GetExpectedDiagnosticResult(int line, int col)
+        {
+            return new DiagnosticResult
+            {
+                Id = "INTL0003",
+                Message = "Attributes should be on separate lines",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", line, col)
+                        }
+            };
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new CodeFixes.AttributesOnSeparateLines();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new Analyzers.AttributesOnSeparateLines();
+        }
+    }
+}
---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/Analyzers/AttributesOnSeparateLines.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IntelliTectAnalyzer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AttributesOnSeparateLines : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "INTL0003";
+        private const string Title = "Attributes separate lines";
+        private const string MessageFormat = "Attributes should be on separate lines";
+        private const string Description = "All attributes should be on separate lines and be wrapped in their own braces.";
+        private const string Category = "Formatting";
+        private const string HelpLinkUri = "https://github.com/IntelliTect/CodingStandards";
+
+        private static readonly DiagnosticDescriptor _Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context is null)
+            {
+                throw new System.ArgumentNullException(nameof(context));
+            }
+
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Property, SymbolKind.NamedType, SymbolKind.Method, SymbolKind.Field);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            ISymbol namedTypeSymbol = context.Symbol;
+
+            if (namedTypeSymbol.GetAttributes().Any())
+            {
+                IDictionary<int, AttributeData> lineDict = new Dictionary<int, AttributeData>();
+                foreach (AttributeData attribute in namedTypeSymbol.GetAttributes())
+                {
+                    SyntaxReference applicationSyntaxReference = attribute.ApplicationSyntaxReference;
+                    Microsoft.CodeAnalysis.Text.TextSpan textspan = applicationSyntaxReference.Span;
+                    SyntaxTree syntaxTree = applicationSyntaxReference.SyntaxTree;
+                    FileLinePositionSpan linespan = syntaxTree.GetLineSpan(textspan);
+
+                    int lineNo = linespan.StartLinePosition.Line;
+                    if (lineDict.ContainsKey(lineNo))
+                    {
+                        Location location = syntaxTree.GetLocation(textspan);
+                        Diagnostic diagnostic = Diagnostic.Create(_Rule, location, attribute.AttributeClass.Name);
+
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                    else
+                    {
+                        lineDict.Add(lineNo, attribute);
+                    }
+                }
+            }
+        }
+    }
+}
