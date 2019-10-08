d42b2f4c003bc9ab860729f87bfee553c4184a01
qtuu <8949946+qtuu@users.noreply.github.com>
2019-10-08 19:31:30 +0200

    Implemented Analyzer and CodeFix
    added unittests to ensure proper function

---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/Analyzers/AttributesOnSeparateLines.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IntelliTectAnalyzer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AttributesOnSeparateLines : DiagnosticAnalyzer
+    {
+        public const string _DiagnosticId = "INTL0003";
+        private const string _Title = "Attributes separate lines";
+        private const string _MessageFormat = "Attributes should be on separate lines";
+        private const string _Description = "All attributes should be on separate lines and be wrapped in their own braces.";
+        private const string _Category = "Formatting";
+        private const string _HelpLinkUri = "https://github.com/IntelliTect/CodingStandards";
+
+        private static readonly DiagnosticDescriptor _Rule = new DiagnosticDescriptor(_DiagnosticId, _Title, _MessageFormat,
+            _Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: _Description, _HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Property, SymbolKind.NamedType, SymbolKind.Method, SymbolKind.Field);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            ISymbol namedTypeSymbol = context.Symbol;
+
+            if (namedTypeSymbol.GetAttributes().Any())
+            {
+                IDictionary<int, AttributeData> lineDict = new Dictionary<int, AttributeData>();
+                foreach (AttributeData attribute in namedTypeSymbol.GetAttributes())
+                {
+                    SyntaxReference applicationSyntaxReference = attribute.ApplicationSyntaxReference;
+                    Microsoft.CodeAnalysis.Text.TextSpan textspan = applicationSyntaxReference.Span;
+                    SyntaxTree syntaxTree = applicationSyntaxReference.SyntaxTree;
+                    FileLinePositionSpan linespan = syntaxTree.GetLineSpan(textspan);
+
+                    int lineNo = linespan.StartLinePosition.Line;
+                    if (lineDict.ContainsKey(lineNo))
+                    {
+                        Location location = syntaxTree.GetLocation(textspan);
+                        Diagnostic diagnostic = Diagnostic.Create(_Rule, location, attribute.AttributeClass.Name);
+
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                    else
+                    {
+                        lineDict.Add(lineNo, attribute);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/CodeFixes/AttributesOnSeparateLines.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace IntelliTectAnalyzer.CodeFixes
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AttributesOnSeparateLines)), Shared]
+    public class AttributesOnSeparateLines : CodeFixProvider
+    {
+        private const string _Title = "Fix Format Violation: Put Attributes on separate Lines";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(Analyzers.AttributesOnSeparateLines._DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start);
+
+            // Find the enclosing AttributeList
+            var attributeList = declaration.Parent;
+            while (attributeList.Kind() != SyntaxKind.AttributeList)
+            {
+                attributeList = attributeList.Parent;
+            }
+
+            // Get the class, method or property adjacent to the AttributeList
+            var parentDeclaration = attributeList.Parent;
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: _Title,
+                    createChangedDocument: c => PutOnSeparateLine(context.Document, parentDeclaration, c),
+                    equivalenceKey: _Title),
+                diagnostic);
+        }
+
+        private async Task<Document> PutOnSeparateLine(Document document, SyntaxNode parentDeclaration, CancellationToken cancellationToken)
+        {
+            var attributeLists = new SyntaxList<AttributeListSyntax>();
+
+            // put every attribute into it's own attributelist eg.: [A,B,C] => [A][B][C]
+            foreach (AttributeSyntax attribute in GetAttributeListSyntaxes(parentDeclaration).SelectMany(l => l.Attributes))
+            {
+                attributeLists = attributeLists.Add(
+                    SyntaxFactory.AttributeList(
+                        SyntaxFactory.SeparatedList<AttributeSyntax>(
+                            new[] {
+                                    SyntaxFactory.Attribute(
+                                        attribute.Name,
+                                        attribute.ArgumentList)
+                            })));
+            }
+
+            // the formatter-annotation will wrap every attribute on a separate line
+            SyntaxNode newNode = BuildNodeWithAttributeLists(parentDeclaration, attributeLists)
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+            // Replace the old local declaration with the new local declaration.
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = oldRoot.ReplaceNode(parentDeclaration, newNode);
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+
+        private IEnumerable<AttributeListSyntax> GetAttributeListSyntaxes(SyntaxNode node)
+        {
+            switch (node)
+            {
+                case ClassDeclarationSyntax c:
+                    return c.AttributeLists;
+                case MethodDeclarationSyntax m:
+                    return m.AttributeLists;
+                case PropertyDeclarationSyntax p:
+                    return p.AttributeLists;
+                case FieldDeclarationSyntax f:
+                    return f.AttributeLists;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private SyntaxNode BuildNodeWithAttributeLists(SyntaxNode node, SyntaxList<AttributeListSyntax> attributeLists)
+        {
+            switch (node)
+            {
+                case ClassDeclarationSyntax c:
+                    return c.WithAttributeLists(attributeLists);
+                case MethodDeclarationSyntax m:
+                    return m.WithAttributeLists(attributeLists);
+                case PropertyDeclarationSyntax p:
+                    return p.WithAttributeLists(attributeLists);
+                case FieldDeclarationSyntax f:
+                    return f.WithAttributeLists(attributeLists);
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
