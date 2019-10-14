d2ed1ae8ec7df84cea740f4d097ab8ec4b52fe65
Bartłomiej Rogowski <brogowski@users.noreply.github.com>
2019-10-14 21:22:35 +0200

    Resolve #7 - New Async Void analyzer (#45)
    * Add Async Void analyzer
    
    * Change async void rule id to INTL0201
    
    * Fix-up unit test

---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer.Test/AsyncVoidTests.cs
@@ -0,0 +1,114 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace IntelliTectAnalyzer.Tests
+{
+    [TestClass]
+    public class AsyncVoidTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void AsyncTaskMethod_NoDiagnosticInformationReturned()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public async Task Sample() { }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(test);
+        }
+
+        [TestMethod]
+        public void AsyncVoidMethod_Warning()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public async void Sample() { }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "INTL0201",
+                Message = "Async methods should not return void.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 13, 31)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [TestMethod]
+        public async Task AsyncVoidMethod_CodeFix_ChangesReturnTypeToTask()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public async void Sample() { }
+        }
+    }";
+
+            var fixTest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public async Task Sample() { }
+        }
+    }";
+            await VerifyCSharpFix(test, fixTest, allowNewCompilerDiagnostics: true);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new CodeFixes.AsyncVoid();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new Analyzers.AsyncVoid();
+        }
+    }
+}
---
+++ IntelliTectAnalyzer/IntelliTectAnalyzer/Analyzers/AsyncVoid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IntelliTectAnalyzer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AsyncVoid : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "INTL0201";
+        private const string Title = "Async void methods";
+        private const string MessageFormat = "Async methods should not return void.";
+        private const string Description = "Async methods must return either Task or Task<T>.";
+        private const string Category = "Design";
+        private const string HelpLinkUri = "https://github.com/IntelliTect/CodingStandards";
+
+        private static readonly DiagnosticDescriptor _Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, 
+            Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description, HelpLinkUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var methodSymbol = context.Symbol as IMethodSymbol;
+
+            if (methodSymbol.IsAsync && methodSymbol.ReturnsVoid)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(_Rule, methodSymbol.Locations[0], methodSymbol.Name));
+            }
+        }
+    }
+}
