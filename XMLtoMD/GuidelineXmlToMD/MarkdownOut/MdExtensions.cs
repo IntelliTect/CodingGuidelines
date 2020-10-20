d92eaf33edec252507d5fd4b4245039aa6f2610c
twofingerrightclick <austensaffron@gmail.com>
2020-10-20 14:01:47 -0700

    Moved MarkdownOut files to the XML project
    The markdown out proj was framework 4.5

---
+++ XMLtoMD/GuidelineXmlToMD/MarkdownOut/MdExtensions.cs
@@ -0,0 +1,36 @@
+﻿namespace MarkdownOut {
+
+    /// <summary>
+    /// A container for extension methods related to Markdown styling and formatting.
+    /// </summary>
+    public static class MdExtensions {
+
+        /// <summary>
+        /// Styles one or more substrings of the provided string using the specified
+        /// <see cref="MdStyle"/>.
+        /// </summary>
+        /// <param name="str">The string containing the substring to style.</param>
+        /// <param name="substring">The substring to style.</param>
+        /// <param name="style">The Markdown style to apply.</param>
+        /// <param name="firstOnly">
+        /// If true, only the first occurrence of the substring is styled; otherwise, all
+        /// occurrences of the substring are styled.
+        /// </param>
+        /// <returns>
+        /// The selectively styled string. If <paramref name="str"/> does not contain any
+        /// occurrences of <paramref name="substring"/>, it is returned unchanged.
+        /// </returns>
+        public static string StyleSubstring(this string str, string substring, MdStyle style,
+                                            bool firstOnly = false) {
+            if (!firstOnly) {
+                return str.Replace(substring, MdText.Style(substring, style));
+            }
+            int pos = str.IndexOf(substring);
+            if (pos < 0) {
+                return str;
+            }
+            return str.Substring(0, pos) + MdText.Style(substring, style)
+                   + str.Substring(pos + substring.Length);
+        }
+    }
+}
---
+++ XMLtoMD/GuidelineXmlToMD/MarkdownOut/MdStyle.cs
@@ -0,0 +1,40 @@
+﻿namespace MarkdownOut {
+
+    /// <summary>
+    /// Specifies a Markdown style to apply to a string of text. Styles are applied by wrapping text
+    /// with a special string on each side and can be selectively applied to substrings.
+    /// </summary>
+    public enum MdStyle {
+
+        /// <summary>
+        /// No text styling.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Italic text styling (surrounds text with a single <c>*</c> character on each side).
+        /// </summary>
+        Italic,
+
+        /// <summary>
+        /// Bold text styling (surrounds text with two <c>*</c> characters on each side).
+        /// </summary>
+        Bold,
+
+        /// <summary>
+        /// Bold italic text styling (surrounds text with three <c>*</c> characters on each side).
+        /// </summary>
+        BoldItalic,
+
+        /// <summary>
+        /// Code text styling (surrounds text with a single <c>`</c> character on each side).
+        /// </summary>
+        Code,
+
+        /// <summary>
+        /// Strike-through text styling (surrounds text with two <c>~</c> characters on each side).
+        /// This style may not be supported by all Markdown parsers.
+        /// </summary>
+        StrikeThrough,
+    }
+}
