9fab791914a45303a8dbc83c94779164763df75f
twofingerrightclick <austensaffron@gmail.com>
2020-10-28 15:04:28 -0700

    Xml to markdown with Workflow (#132)
    * essential functions of script
    
    * saving md
    
    * saving md
    
    * saving md
    
    * saving md
    
    * saving md
    
    * saving md
    
    checking how it looks on github
    
    * saving md
    
    * Test action
    
    * test action
    
    * Create main.yml
    
    * Update main.yml
    
    * Moved MarkdownOut files to the XML project
    
    The markdown out proj was framework 4.5
    
    * Update main.yml
    
    * use system specific set path directory seperator for file access
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * remove extra files and rename output MD file
    
    * Update and rename main.yml to ConvertGuidelinesXmlToMarkdown.yml
    
    * Update ConvertGuidelinesXmlToMarkdown.yml
    
    * Print C# guidelines as a heading at top of file
    
    * Update ConvertGuidelinesXmlToMarkdown.yml
    
    * Update ConvertGuidelinesXmlToMarkdown.yml
    
    * new coding guidelines MD File created
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>
    
    * Added IntelliTect Analyzers and suppressed some null string warnings.
    
    The null public parameter warnings were apart of code copied from an old project. This code isn't going to be used in production, so I didn't refactor.
    
    * Guideline class now has correct GetHashCode and Equals methods
    
    * Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Trying to figure out how to have a string env variable for the job
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    Update ConvertGuidelinesXmlToMarkdown.yml
    positive logic 
    
    git hub actions proceed only if the xml file was modified in the previous commit ( git diff HEAD~1 HEAD)
    get xml filename as argument
    
    
    Update ConvertGuidelinesXmlToMarkdown.yml
    
    * Use simple-diff action
    
    * Update ConvertGuidelinesXmlToMarkdown.yml
    
    Co-authored-by: twofingerrightclick <twofingerrightclick@users.noreply.github.com>
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

---
+++ XMLtoMD/GuidelineXmlToMD/MarkdownOut/MdWriter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+
+namespace MarkdownOut // by https://github.com/rob-williams/MarkdownOut
+{
+
+    /// <summary>
+    /// A wrapper around <see cref="StreamWriter"/> used to write Markdown text to a file.
+    /// </summary>
+    public class MdWriter : IDisposable {
+
+        private StreamWriter _Stream;
+        private bool _IsDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MdWriter"/> class, opening a stream to the
+        /// file at the specified path.
+        /// </summary>
+        /// <param name="path">The full path to the output file, including file extension.</param>
+        /// <param name="append">
+        /// If true, output is appended to the file's existing contents; otherwise, the file is
+        /// overwritten.
+        /// </param>
+        public MdWriter(string path, bool append = false) {
+            _Stream = new StreamWriter(path, append);
+        }
+
+        #region IDisposable
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        // Dispose() calls Dispose(true)
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // The bulk of the clean-up code is implemented in Dispose(bool)
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_IsDisposed) return;
+
+            if (disposing)
+            {
+                // free managed resources
+                _Stream.Dispose();
+            }
+
+            // free native resources if there are any.
+        
+            _IsDisposed = true;
+        }
+
+
+        #endregion
+
+
+        /// <summary>
+        /// Writes the provided output to the file.
+        /// </summary>
+        /// <param name="output">The output to write.</param>
+        /// <param name="style">The optional Markdown style to apply.</param>
+        /// <param name="format">The optional Markdown format to apply.</param>
+        /// <param name="useMdLineBreaks">
+        /// If true, when the text is cleansed, all newlines will be replaced by Markdown line
+        /// breaks to maintain the assumed intended line breaks in the text; otherwise, the text's
+        /// newlines will not parsed as line breaks by Markdown parsers.
+        /// </param>
+        public void Write(object output, MdStyle style = MdStyle.None,
+                          MdFormat format = MdFormat.None, bool useMdLineBreaks = true) {
+            string text = MdText.StyleAndFormat(output, style, format);
+            _Stream.Write(MdText.Cleanse(text, useMdLineBreaks));
+        }
+
+        /// <summary>
+        /// Writes the provided output to the file, followed by a Markdown paragraph break to
+        /// terminate the line and start a new paragraph.
+        /// </summary>
+        /// <param name="output">The output to write.</param>
+        /// <param name="style">The optional Markdown style to apply.</param>
+        /// <param name="format">The optional Markdown format to apply.</param>
+        /// <param name="useMdLineBreaks">
+        /// If true, when the text is cleansed, all newlines will be replaced by Markdown line
+        /// breaks to maintain the assumed intended line breaks in the text; otherwise, the text's
+        /// newlines will not parsed as line breaks by Markdown parsers.
+        /// </param>
+        public void WriteLine(object output, MdStyle style = MdStyle.None,
+                              MdFormat format = MdFormat.None, bool useMdLineBreaks = true, int numNewLines = 2) {
+            string text = MdText.StyleAndFormat(output, style, format);
+            _Stream.Write(MdText.Cleanse(text, useMdLineBreaks) + MakeParagraphLineBreak(numNewLines));
+        }
+
+        /// <summary>
+        /// Writes the provided output to the file, followed by a Markdown line break to terminate
+        /// the line but not start a new paragraph.
+        /// </summary>
+        /// <param name="output">The output to write.</param>
+        /// <param name="style">The optional Markdown style to apply.</param>
+        /// <param name="format">
+        /// The optional Markdown format to apply. Some formats may not be terminated by the
+        /// Markdown line break.
+        /// </param>
+        /// <param name="useMdLineBreaks">
+        /// If true, when the text is cleansed, all newlines will be replaced by Markdown line
+        /// breaks to maintain the assumed intended line breaks in the text; otherwise, the text's
+        /// newlines will not parsed as line breaks by Markdown parsers.
+        /// </param>
+        public void WriteLineSingle(object output, MdStyle style = MdStyle.None,
+                                    MdFormat format = MdFormat.None, bool useMdLineBreaks = true) {
+            string text = MdText.StyleAndFormat(output, style, format);
+            _Stream.Write(MdText.Cleanse(text, useMdLineBreaks) + MdText.LineBreak);
+        }
+
+        /// <summary>
+        /// Writes the provided output to the file using the
+        /// <see cref="MdFormat.UnorderedListItem"/> format, followed by a Markdown paragraph break
+        /// to terminate the list item.
+        /// </summary>
+        /// <param name="output">The output to write.</param>
+        /// <param name="listIndent">
+        /// The optional indent of the list item (0 adds no indent, 1 adds a single indentation to
+        /// create a sublist, etc). Negative values are ignored.
+        /// </param>
+        /// <param name="style">The optional Markdown style to apply.</param>
+
+        /// <param name="format">The optional Markdown format to apply.</param>
+        /// </param>
+        public void WriteUnorderedListItem(object output, int listIndent = 0,
+                                           MdStyle style = MdStyle.None, MdFormat format = MdFormat.None, int numNewLines=1) {
+            string text = MdText.Format(output, format);
+            text = MdText.StyleAndFormat(text, style, MdFormat.UnorderedListItem);
+            text = MdText.Indent(text, listIndent);
+            _Stream.Write(MdText.Cleanse(text, true)
+                + MakeParagraphLineBreak(numNewLines));
+        }
+
+        private static string MakeParagraphLineBreak(int numNewLines)
+        {
+            string breaks = "";
+            for (int i = 0; i < numNewLines; i++)
+            {
+                breaks = breaks + MdText.ParagraphBreakOneLine;
+            }
+            return breaks;
+        }
+
+
+
+        /// <summary>
+        /// Writes the provided output to the file using the <see cref="MdFormat.OrderedListItem"/>
+        /// format, followed by a Markdown paragraph break to terminate the list item.
+        /// </summary>
+        /// <param name="output">The output to write.</param>
+        /// <param name="itemNumber">
+        /// The optional item number of the list item. Does not affect parsed Markdown output or the
+        /// list order, only the raw text. Negative values are ignored.
+        /// </param>
+        /// <param name="listIndent">
+        /// The optional indent of the list item (0 adds no indent, 1 adds a single indentation to
+        /// create a sublist, etc). Negative values are ignored.
+        /// </param>
+        /// <param name="style">The optional Markdown style to apply.</param>
+        public void WriteOrderedListItem(object output, int itemNumber = 1, int listIndent = 0,
+                                         MdStyle style = MdStyle.None) {
+            string text = MdText.StyleAndFormat(output, style, MdFormat.OrderedListItem);
+            //replace the list item number supplied by MdText with the number provided
+            if (itemNumber != MdText.DefaultListItemNumber && itemNumber >= 0) {
+                text = itemNumber + text.Substring(1);
+            }
+            text = MdText.Indent(text, listIndent);
+            _Stream.Write(MdText.Cleanse(text, true) + MdText.ParagraphBreak);
+        }
+    }
+}
---
+++ XMLtoMD/GuidelineXmlToMD/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using MarkdownOut;
+
+
+namespace GuidelineXmlToMD
+{
+    class Program
+    {
+        static MarkdownOut.MdWriter _MdWriter;
+        static void Main(string[] args)
+        {
+            string xmlFileName = "Guidelines(8th Edition).xml";
+            if (args.Length != 0) {  //check for input fileName
+                if (Regex.Match(args[0], @".*.xml").Success) {
+                    xmlFileName = args[0];
+                }
+            }
+
+            Match repoRefFolder = Regex.Match(AssemblyDirectory, @$".*CodingGuidelines");
+
+            string[] xmlFilePath = { repoRefFolder.Value, "docs", xmlFileName};
+            ICollection<Guideline> guidelines = GuidelineXmlFileReader.ReadExisitingGuidelinesFile(Path.Combine(xmlFilePath));
+            
+            
+            string mdFileName = "csharp.md";
+            string[] mdFilePath = { repoRefFolder.Value, "docs", "coding", mdFileName};
+
+            using (_MdWriter = new MdWriter(Path.Combine(mdFilePath)))
+            {
+
+
+                _MdWriter.WriteLine("C# Guidelines", format: MdFormat.Heading1);
+                PrintSections(guidelines);
+                _MdWriter.WriteLine("Guidelines", format: MdFormat.Heading1);
+                _MdWriter.WriteLine("");
+                PrintGuidelinesBySection(guidelines);
+            }
+        
+
+
+
+            //C: \Users\saffron\source\repos\CodingGuidelines\XMLtoMD\GuidelineXmlToMD\bin\Debug\netcoreapp3.1
+            //C: \Users\saffron\source\repos\CodingGuidelines\docs
+
+
+        }
+
+        private static void PrintGuidelinesBySection(ICollection<Guideline> guidelines)
+        {
+            foreach (string section in GetSections(guidelines))
+            {
+                _MdWriter.WriteLine("");
+                Console.WriteLine(section);
+                _MdWriter.WriteLine(section, format: MdFormat.Heading2,style: MdStyle.BoldItalic);
+
+
+
+                IEnumerable<Guideline> guidelinesInSections = (from guideline in guidelines
+                                                               where string.Equals(guideline.Section, section, StringComparison.OrdinalIgnoreCase)
+                                                               select guideline);
+
+                foreach (string subsection in GetSubSections(guidelines))
+                {
+
+                    IEnumerable<Guideline> guidelinesInSubsection = (from guideline in guidelinesInSections
+                                                                     where string.Equals(guideline.Subsection, subsection, StringComparison.OrdinalIgnoreCase)
+                                                                     select guideline);
+
+                    if (guidelinesInSubsection.Any())
+                    {
+                        Console.WriteLine($"     { subsection}");
+                        _MdWriter.WriteLine(subsection, format: MdFormat.Heading3, numNewLines: 1);
+
+                        foreach (Guideline guideline in guidelinesInSubsection)
+                        {
+                            _MdWriter.WriteUnorderedListItem(GetGuidelineEmoji(guideline) + " " + guideline.Text.Trim('"'), listIndent: 0);
+                        }
+                    }
+                    _MdWriter.WriteLine("", numNewLines: 1);
+
+                }
+            }
+        }
+
+        private static string GetGuidelineEmoji(Guideline guideline)
+        {
+            string emoji = "";
+            switch (guideline.Severity)
+            {
+                case "AVOID":
+                    emoji = ":no_entry:";
+                    break;
+                case "DO NOT":
+                    emoji = ":x:";
+                    break;
+                case "DO":
+                    emoji = ":heavy_check_mark:";
+                    break;
+                case "CONSIDER":
+                    emoji = ":grey_question:";
+                    break;
+
+                default:
+                    break;
+            }
+            return emoji;
+        }
+
+        private static void PrintSections(ICollection<Guideline> guidelines)
+        {
+            _MdWriter.WriteLine("Sections", format: MdFormat.Heading2);
+
+            List<string> subSections = new List<string>();
+
+            List<string> sections = GetSections(guidelines);
+            foreach (string section in sections)
+            {
+                Console.WriteLine(section);
+
+                _MdWriter.WriteUnorderedListItem(section, format: MdFormat.InternalLink, listIndent: 0);
+
+
+                subSections = (from guideline in guidelines
+                               where string.Equals(guideline.Section, section, System.StringComparison.OrdinalIgnoreCase)
+                               select guideline.Subsection).Distinct().OrderBy(x => x).ToList();
+
+                foreach (string subsection in subSections)
+                {
+                    Console.WriteLine($"     { subsection}");
+                    _MdWriter.WriteUnorderedListItem(subsection, format: MdFormat.InternalLink, listIndent: 1);
+                }
+                _MdWriter.WriteLine("", numNewLines: 1);
+
+
+
+            }
+
+
+
+        }
+
+        public static List<string> GetSections(ICollection<Guideline> guidelines)
+        {
+
+            List<string> sections = (from guideline in guidelines
+                                     select guideline.Section).Distinct().OrderBy(x => x).ToList();
+            return sections;
+        }
+
+        public static List<string> GetSubSections(ICollection<Guideline> guidelines)
+        {
+
+            List<string> subSections = (from guideline in guidelines
+                                        select guideline.Subsection).Distinct().OrderBy(x => x).ToList();
+            return subSections;
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+    }
+
+
+
+
+
+}
