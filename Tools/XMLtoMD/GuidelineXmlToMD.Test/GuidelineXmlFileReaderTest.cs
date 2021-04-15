a28563c1f4511d5345430a956f4b092b2bf4e6a5
Joseph Riddle <joeriddles10@gmail.com>
2021-04-15 16:21:00 +0000

    Add GuidelineXmlToMD mstest project

---
+++ Tools/XMLtoMD/GuidelineXmlToMD.Test/GuidelineXmlFileReaderTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuidelineXmlToMD.Test
+{
+    [TestClass]
+    public class GuidelineXmlFileReaderTest
+    {
+        [TestMethod]
+        public void ReadExisitingGuidelinesFile_ExistingFile_ReadsGuidlines()
+        {
+            // TODO...
+        }
+    }
+}
