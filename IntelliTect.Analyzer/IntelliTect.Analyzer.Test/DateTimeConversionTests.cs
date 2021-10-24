f5afc1eae9ca0c392677df36d060e98611b49d7e
Kelly Adams <kelly@intellitect.com>
2021-10-24 08:26:24 -0700

    Added failing test and stub of analyzer.

---
+++ IntelliTect.Analyzer/IntelliTect.Analyzer.Test/DateTimeConversionTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace IntelliTect.Analyzer.Tests
+{
+    [TestClass]
+    public class DateTimeConversionTests : CodeFixVerifier
+    {
+        [TestMethod]
+        public void UsageOfImplicitConversion_ProducesWarningMessage()
+        {
+            string source = @"
+using System;
+
+namespace ConsoleApp42
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                DateTimeOffset ofs = DateTime.Now;
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(source,
+                           new DiagnosticResult
+                           {
+                               Id = "INTL0304",
+                               Severity = DiagnosticSeverity.Warning,
+                               Message = "Using the symbol 'DateTimeOffset.implicit operator DateTimeOffset(DateTime)' can result in unpredictable behavior.",
+                               Locations =
+                                   new[] {
+                            new DiagnosticResultLocation("Test0.cs", 9, 30)
+                                   }
+                           });
+
+        }
+
+        [TestMethod]
+        public void UsageOfExplicitConversion_ProducesNothing()
+        {
+            string source = @"
+using System;
+
+namespace ConsoleApp43
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                DateTimeOffset ofs = (DateTimeOffset)DateTime.Now;
+            }
+        }
+    }";
+
+            VerifyCSharpDiagnostic(source);
+
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new Analyzers.BanImplicitDateTimeToDateTimeOffsetConversion();
+        }
+    }
+}
---
+++ IntelliTect.Analyzer/IntelliTect.Analyzer/Analyzers/BanImplicitDateTimeToDateTimeOffsetConversion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace IntelliTect.Analyzer.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class BanImplicitDateTimeToDateTimeOffsetConversion : DiagnosticAnalyzer
+    {
+        private const string Category = "Performance";
+
+        private static readonly DiagnosticDescriptor _Rule304 = new DiagnosticDescriptor(Rule304.DiagnosticId,
+            Rule304.Title,
+            Rule304.MessageFormat,
+            Category, DiagnosticSeverity.Warning, true, Rule304.Description,
+            Rule304.HelpMessageUri);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule304);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+            context.EnableConcurrentExecution();
+            //context.RegisterSyntaxNodeAction(AnalyzeInvocation, SyntaxKind.InvocationExpression);
+        }
+
+        private static class Rule304
+        {
+            internal const string DiagnosticId = "INTL0304";
+            internal const string Title = "Do not use implicit conversion between `DateTime` and `DateTimeOffset`";
+            internal const string MessageFormat = "Using the symbol 'DateTimeOffset.implicit operator DateTimeOffset(DateTime)' can result in unpredictable behavior.";
+#pragma warning disable INTL0001 // Allow field to not be prefixed with an underscore to match the style
+            internal static readonly string HelpMessageUri = DiagnosticUrlBuilder.GetUrl(Title,
+                DiagnosticId);
+#pragma warning restore INTL0001 
+
+            internal const string Description =
+                "When you use EnumerateFiles, you can start enumerating the collection of names before the whole collection is returned; when you use GetFiles, you must wait for the whole array of names to be returned before you can access the array. Therefore, when you are working with many files and directories, EnumerateFiles can be more efficient.";
+        }
+    }
+}
