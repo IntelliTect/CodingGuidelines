5025a61b3d15e70cebbe162d237e0e0753048826
Kevin B <Keboo@users.noreply.github.com>
2021-11-03 14:27:34 -0700

    Moving DateTime conversion analyzer to Reliability

--- IntelliTect.Analyzer/IntelliTect.Analyzer/Analyzers/BanImplicitDateTimeToDateTimeOffsetConversion.cs
+++ IntelliTect.Analyzer/IntelliTect.Analyzer/Analyzers/BanImplicitDateTimeToDateTimeOffsetConversion.cs
@@ -9,15 +9,15 @@ namespace IntelliTect.Analyzer.Analyzers
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class BanImplicitDateTimeToDateTimeOffsetConversion : DiagnosticAnalyzer
     {
-        private const string Category = "Performance";
+        private const string Category = "Reliability";
 
-        private static readonly DiagnosticDescriptor _Rule304 = new DiagnosticDescriptor(Rule304.DiagnosticId,
-            Rule304.Title,
-            Rule304.MessageFormat,
-            Category, DiagnosticSeverity.Warning, true, Rule304.Description,
-            Rule304.HelpMessageUri);
+        private static readonly DiagnosticDescriptor _Rule202 = new DiagnosticDescriptor(Rule202.DiagnosticId,
+            Rule202.Title,
+            Rule202.MessageFormat,
+            Category, DiagnosticSeverity.Warning, true, Rule202.Description,
+            Rule202.HelpMessageUri);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule304);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(_Rule202);
 
         public override void Initialize(AnalysisContext context)
         {
@@ -44,16 +44,16 @@ namespace IntelliTect.Analyzer.Analyzers
                 INamedTypeSymbol dateTimeOffsetType = context.Compilation.GetTypeByMetadataName("System.DateTimeOffset");
                 if (containingType.Equals(dateTimeOffsetType))
                 {
-                    context.ReportDiagnostic(Diagnostic.Create(_Rule304, conversionOperation.Syntax.GetLocation()));
+                    context.ReportDiagnostic(Diagnostic.Create(_Rule202, conversionOperation.Syntax.GetLocation()));
                 }
             }
 
 
         }
 
-        private static class Rule304
+        private static class Rule202
         {
-            internal const string DiagnosticId = "INTL0304";
+            internal const string DiagnosticId = "INTL0202";
             internal const string Title = "Do not use implicit conversion from `DateTime` to `DateTimeOffset`";
             internal const string MessageFormat = "Using the symbol 'DateTimeOffset.implicit operator DateTimeOffset(DateTime)' can result in unpredictable behavior.";
 #pragma warning disable INTL0001 // Allow field to not be prefixed with an underscore to match the style
