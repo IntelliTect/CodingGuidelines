c382bcb374e5749c17340a6e69f26e28c4bc2324
joeriddles <joeriddles10@gmail.com>
2024-01-09 09:26:50 -0800

    Fix failing test line

--- IntelliTect.Analyzer/IntelliTect.Analyzer.Test/DateTimeConversionTests.cs
+++ IntelliTect.Analyzer/IntelliTect.Analyzer.Test/DateTimeConversionTests.cs
@@ -64,7 +64,7 @@ namespace ConsoleApp1
                                Id = "INTL0202",
                                Severity = DiagnosticSeverity.Warning,
                                Message = "Using the symbol 'DateTimeOffset.implicit operator DateTimeOffset(DateTime)' can result in unpredictable behavior",
-                               Locations = [new DiagnosticResultLocation("Test0.cs", 17, 17)]
+                               Locations = [new DiagnosticResultLocation("Test0.cs", 13, 17)]
                            });
 
         }
